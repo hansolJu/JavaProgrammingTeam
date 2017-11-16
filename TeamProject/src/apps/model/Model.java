29ec2ee28a187882864810bd104cb39975bcd900
eunjin <eunjin@LAPTOP-4LQVBRJ5>
2017-11-16 14:52:32 +0900

    지훈아 컨트롤러안의 compile, save errors 이벤트짜줘

--- TeamProject/src/apps/model/Model.java
+++ TeamProject/src/apps/model/Model.java
@@ -3,9 +3,16 @@ package apps.model;
 public class Model {
 	private String filePath;
 	private String fileName;
+	private String fileDir;
 	
 	public String getFilePath() {
 		return filePath;
+	}
+	public String getFileDir() {
+		return fileDir;
+	}
+	public void setFileDir(String fileDir) {
+		this.fileDir = fileDir;
 	}
 	public void setFilePath(String filePath) {
 		this.filePath = filePath;
