1d0cf0dc861c9cf2a8616ef5993abb7b87f5184f
eunjin <eunjin@LAPTOP-4LQVBRJ5>
2017-11-16 13:12:41 +0900

    모델 클래스 작성

---
+++ TeamProject/src/apps/model/Model.java
@@ -0,0 +1,19 @@
+package apps.model;
+
+public class Model {
+	private String filePath;
+	private String fileName;
+	
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+}
