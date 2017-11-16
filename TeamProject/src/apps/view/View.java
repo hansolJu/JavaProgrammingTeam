4ff5bf336d260417e7aa5de3109a956948152621
컴퓨터 <@DESKTOP-C01FUL7>
2017-11-16 16:53:31 +0900

    뷰단 수정

--- TeamProject/src/apps/view/View.java
+++ TeamProject/src/apps/view/View.java
@@ -15,6 +15,8 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class View {
 	private JFrame frame;
@@ -23,6 +25,7 @@ public class View {
 	private JButton openButton;
 	private JButton saveButton;
 	private JButton compileButton;
+	private JButton runButton;
 	private JButton saveErrorButton;
 	private JButton deleteButton;
 	private JButton clearButton;
@@ -37,50 +40,84 @@ public class View {
 		//Create the principal frame
 		frame  = new JFrame(title);
 		frame.getContentPane().setLayout(new BorderLayout());
+		frame.setBounds(100, 100, 500, 782);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 100);
 		frame.setLocationRelativeTo(null);//프레임 중앙에 배치.
 		frame.setVisible(true);
 		
 		//Create UI elements
-		openFilePath = new JTextField(20);
-		saveFilePath = new JTextField(20);
+		openFilePath = new JTextField();
+		openFilePath.setColumns(10);
+		saveFilePath = new JTextField();
+		saveFilePath.setColumns(10);
+		
 		openButton = new JButton("Open");
 		saveButton = new JButton("Save");
 		compileButton = new JButton("Compile");
+		runButton = new JButton("Run");
 		saveErrorButton = new JButton("SaveError");
 		deleteButton = new JButton("Delete");
 		clearButton = new JButton("Clear");
+		
 		editingWindowArea = new JTextArea();
 		resultWindowArea = new JTextArea();
 		
 		//Add UI element to frame
-		GroupLayout layout = new GroupLayout(frame.getContentPane());
-		layout.setAutoCreateGaps(true);//컨테이너와 컨테이너의 경계에 접하는 컴퍼넌트 간의 간격이 자동으로 생성할지 여부를 설정합니다
-		layout.setAutoCreateContainerGaps(true);//컨테이너와 컨테이너의 경계에 접하는 컴퍼넌트 간의 간격이 자동으로 생성할지 여부를 설정합니다.
-		layout.setHorizontalGroup(layout.createSequentialGroup()
-							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-								.addComponent(openFilePath)
-								.addComponent(openButton)
-							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-								.addComponent(saveFilePath)
-								.addComponent(saveButton)
-							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-								.addComponent(editingWindowArea)
-							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-								.addComponent(compileButton)
-								.addComponent(saveErrorButton)
-								.addComponent(deleteButton)
-								.addComponent(clearButton)
-							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
-								.addComponent(resultWindowArea)
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(resultWindowArea, GroupLayout.DEFAULT_SIZE, 485, Short.MAX_VALUE)
+						.addComponent(editingWindowArea, GroupLayout.DEFAULT_SIZE, 485, Short.MAX_VALUE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(compileButton, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(runButton, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(saveErrorButton, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(deleteButton, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(clearButton, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(openFilePath, GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE)
+								.addComponent(saveFilePath, GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(saveButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(openButton, GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE))
+							.addGap(6)))
+					.addContainerGap())
 		);
-		
-		layout.linkSize(SwingConstants.HORIZONTAL,openButton,saveButton,compileButton,saveErrorButton,deleteButton,clearButton);
-		layout.linkSize(SwingConstants.HORIZONTAL,openFilePath,saveFilePath);
-		layout.linkSize(SwingConstants.HORIZONTAL,editingWindowArea,resultWindowArea);
-		
-		frame.getContentPane().setLayout(layout);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(openFilePath, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(openButton)
+							.addGap(7)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(saveButton)
+								.addComponent(saveFilePath, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(editingWindowArea, GroupLayout.PREFERRED_SIZE, 278, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(compileButton)
+						.addComponent(clearButton)
+						.addComponent(runButton)
+						.addComponent(saveErrorButton)
+						.addComponent(deleteButton))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(resultWindowArea, GroupLayout.DEFAULT_SIZE, 351, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		frame.getContentPane().setLayout(groupLayout);
 	}
 	/**
 	 * generate a getter and setter
