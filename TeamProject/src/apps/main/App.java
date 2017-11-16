4ae95208fca4164582434e5885c64a0d60d4f458
컴퓨터 <@DESKTOP-C01FUL7>
2017-11-16 15:38:33 +0900

    메인파일

---
+++ TeamProject/src/apps/main/App.java
@@ -0,0 +1,15 @@
+package apps.main;
+
+import apps.controller.Controller;
+import apps.model.Model;
+import apps.view.View;
+
+public class App {
+	public static void main(String[] args) {
+		Model m = Model.getInstance();
+		View v = new View("IDE");
+		Controller c = new Controller(m,v);
+		
+	}
+
+}
